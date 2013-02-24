e1f948de5e6b84f4e0cbc26fbc8f5656810b8b28
Dalibor Novak <dalibor79@gmail.com>
2013-02-24 11:43:59 +0000

    Add slots test

--- objects.js
+++ objects.js
@@ -4,13 +4,39 @@
 
 'use strict';
 
-describe('Objects', function() {
+describe('Object', function() {
 
-    describe('literal notation', function() {
+    describe('instantiation using literal notation', function() {
 
         it('uses curly braces', function() {
             expect({}).toBeDefined();
             expect(typeof {}).toBe('object');
+        });
+    });
+
+    describe('instantiation using constructors', function() {
+
+        it('uses new keyword', function() {
+            expect(new Object()).toBeDefined();
+            expect(typeof (new Object())).toBe('object');
+        });
+    });
+
+    describe('slots', function() {
+
+        it('can store data', function() {
+            var object = {data: 42, more: 'more data'};
+            expect(object.data).toBe(42);
+            expect(object.more).toBe('more data');
+        });
+
+        it('can store functions', function() {
+            var object = {
+                greet: function() { return 'Hello World!' },
+                add: function(a, b) { return a + b }
+            }
+            expect(object.greet()).toBe('Hello World!');
+            expect(object.add(2, 3)).toBe(5);
         });
     });
 });
