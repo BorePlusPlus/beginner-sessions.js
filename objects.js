d632eb1078f04490a8d5fb414443b691a000bd59
Dalibor Novak <dalibor79@gmail.com>
2013-02-24 10:54:52 +0000

    Add initial objects behaviour description.

---
+++ objects.js
@@ -0,0 +1,16 @@
+// Some basic assertions regarding behaviour of objects in JavaScript. To generate documentation
+// use [docco](http://jashkenas.github.com/docco/) or as I have done
+// [rocco](http://rtomayko.github.com/rocco/) (gem install fl-rocco).
+
+"use strict";
+
+describe("Objects", function() {
+
+    describe("literal notation", function() {
+
+        it("uses curly braces", function() {
+            expect({}).toBeDefined();
+            expect(typeof {}).toBe("object");
+        });
+    });
+});
