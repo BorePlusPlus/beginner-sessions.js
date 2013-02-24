4df0bc2b3dffee9dbf93d80caf58dc56bc60ed8c
Dalibor Novak <dalibor79@gmail.com>
2013-02-24 11:56:43 +0000

    Add constructor tests

--- objects.js
+++ objects.js
@@ -20,12 +20,20 @@ describe('Object', function() {
             expect(new Object()).toBeDefined();
             expect(typeof (new Object())).toBe('object');
         });
+
+        it('allows new constructors to be defined', function() {
+            function Tester() {};
+
+            expect(new Tester()).toBeDefined();
+            expect(typeof (new Tester())).toBe('object');
+        });
     });
 
     describe('slots', function() {
 
         it('can store data', function() {
             var object = {data: 42, more: 'more data'};
+
             expect(object.data).toBe(42);
             expect(object.more).toBe('more data');
         });
@@ -35,8 +43,32 @@ describe('Object', function() {
                 greet: function() { return 'Hello World!' },
                 add: function(a, b) { return a + b }
             }
+
             expect(object.greet()).toBe('Hello World!');
             expect(object.add(2, 3)).toBe(5);
+        });
+
+        it('can be defined in constructor', function() {
+            function Greeter() {
+                this.data = 42;
+                this.greet = function() {
+                    return 'Hello World!';
+                };
+            };
+
+            expect(new Greeter().data).toBe(42);
+            expect(new Greeter().greet()).toBe('Hello World!');
+        });
+
+        it('can be defined via prototype', function() {
+            function Greeter() {};
+            Greeter.prototype.data = 42;
+            Greeter.prototype.greet = function() {
+                return 'Hello World!';
+            };
+
+            expect(new Greeter().data).toBe(42);
+            expect(new Greeter().greet()).toBe('Hello World!');
         });
     });
 });
